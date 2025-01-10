06b78897ab512a6674ae71ddc6db89325e7ac20c
Saba Safavi <sabasfvi@gmail.com>
2025-01-10 13:27:18 +0330

    21day

---
+++ tamrinatSabzlearn/js/e1.js
@@ -0,0 +1,11 @@
+let FirstNumber = prompt("please enter the first number");
+let SecondNumber = prompt("please enter the second number");
+let NumericFirstNumber = +FirstNumber;
+let NumericSecondNumber = +SecondNumber;
+let plusNumber=NumericFirstNumber+NumericSecondNumber;
+alert(plusNumber);
+alert(NumericFirstNumber-NumericSecondNumber);
+alert(NumericFirstNumber*NumericSecondNumber)
+alert(NumericFirstNumber**NumericSecondNumber)
+alert(NumericFirstNumber/NumericSecondNumber)
+alert(NumericFirstNumber%NumericSecondNumber)
