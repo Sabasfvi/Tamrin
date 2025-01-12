5834aa141474413a59568ea9808e4448c3360442
Saba Safavi <sabasfvi@gmail.com>
2025-01-12 11:49:41 +0330

    23DayMah

---
+++ tamrinatSabzlearn/js/average.js
@@ -0,0 +1,6 @@
+let UserNumber1=+prompt("please enter the first number",0);
+let UserNumber2=+prompt("please enter the Second number",0);
+let UserNumber3=+prompt("please enter the Third number",0);
+let  Sum=(UserNumber1+UserNumber2+UserNumber3);
+let  Average=(Sum/3);
+alert(Average);
