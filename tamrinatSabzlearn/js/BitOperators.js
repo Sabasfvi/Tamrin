7acc6512685d52d130278695ee7ba78f3be808b7
Saba Safavi <sabasfvi@gmail.com>
2025-01-15 10:01:27 +0330

    26Daymah

---
+++ tamrinatSabzlearn/js/BitOperators.js
@@ -0,0 +1,8 @@
+let AgeUser=+prompt("How Old Are You?","18");
+let GenderUser=prompt("What Is Your Gender?","Male OR Female");
+
+if(AgeUser<18 || GenderUser==="Female"){
+    alert("ERROR");
+}else {
+    alert("Welcome To Your Panel");
+}
