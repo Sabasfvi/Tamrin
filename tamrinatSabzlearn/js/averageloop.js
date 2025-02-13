8a7ec718ea3d50fb3111a14a67e4aa06a9ee087e
Saba Safavi <sabasfvi@gmail.com>
2025-02-13 12:23:45 +0330

    25BAHMAN

---
+++ tamrinatSabzlearn/js/averageloop.js
@@ -0,0 +1,5 @@
+let sum=0;
+for(let i=1;i<=5;i++){
+    let UserNumber=+prompt("Enter number: "+" "+i);
+    sum+=UserNumber;
+}alert("the average is:"+sum/5)
--- tamrinatSabzlearn/js/trace2.js
+++ tamrinatSabzlearn/js/trace2.js
@@ -1,9 +1,17 @@
 
-let  UserNumber=prompt("Enter Your Number");
+//روش خودم
+// let  UserNumber=prompt("Enter Your Number");
+// let sum=0;
+// for(let i=0;i<UserNumber.length;i++){
+//       sum=sum+Number(UserNumber[i]);
+//
+// }alert(sum)
+//------------------------------------------------------------------------------//
+//روش استاد:
+let UserNumber=+prompt("please enter your number"); //123
 let sum=0;
-for(let i=0;i<UserNumber.length;i++){
-        sum += UserNumber[i];
-
-
-
-} 
+for (let i = 0; Math.floor(UserNumber/10)!==0; i++) {
+     sum=sum+(UserNumber%10);
+     UserNumber=Math.floor(UserNumber/10);
+}
+alert(sum);
