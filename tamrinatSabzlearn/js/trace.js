04396fc54783ec68f537d088d8fa7d1e18bf1788
Saba Safavi <sabasfvi@gmail.com>
2025-02-13 09:37:33 +0330

    25Bahman

---
+++ tamrinatSabzlearn/js/trace.js
@@ -0,0 +1,8 @@
+
+let sum=0;
+
+for(let i=0;i<5;i++){
+    let price=+prompt("قیمت محصول"+i+"ام را وارد کنید")
+     sum+=price
+}
+alert(" "+" قیمت کل سبدخرید برابر"+ sum)
---
+++ tamrinatSabzlearn/js/trace2.js
@@ -0,0 +1,9 @@
+
+let  UserNumber=prompt("Enter Your Number");
+let sum=0;
+for(let i=0;i<UserNumber.length;i++){
+        sum += UserNumber[i];
+
+
+
+} 
