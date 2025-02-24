cb3c77374df777277fcb962d39a9275fb1540717
Saba Safavi <sabasfvi@gmail.com>
2025-02-24 11:06:30 +0330

    6esfandmah

---
+++ tamrinatSabzlearn/js/testtamrin.js
@@ -0,0 +1,15 @@
+
+let list=document.getElementsByTagName('li');
+list[2].setAttribute('id' , 'about')
+console.log(list)
+
+
+let header2=document.getElementById('welcome');
+header2.style.color = 'red';
+header2.style.fontSize = '100px';
+
+
+let input=document.getElementById('input');
+input.value="hello world";
+
+
