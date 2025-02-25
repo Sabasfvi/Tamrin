981a9e600cfe29726d6fb83d2f2ba8f792b3af8d
Saba Safavi <sabasfvi@gmail.com>
2025-02-25 09:27:50 +0330

    7esfandmah

--- tamrinatSabzlearn/js/testtamrin.js
+++ tamrinatSabzlearn/js/testtamrin.js
@@ -12,4 +12,29 @@ header2.style.fontSize = '100px';
 let input=document.getElementById('input');
 input.value="hello world";
 
+let img=document.getElementById('img');
+let button=document.getElementById('lampbutton');
+///on--> true       off-->false
+    let lampFlag=false   ///اول خاموشه و لینک عکس خاموش در صفحه است
+
+    function clickFunc(){
+        if(lampFlag){
+            img.setAttribute('src',"../src/off.jpg")
+            button.innerHTML='on'
+            lampFlag=false
+        }else {
+            img.setAttribute('src',"../src/on.jpg")
+            button.innerHTML='off'
+            lampFlag=true
+
+
+        }
+
+
+}
+
+
+
+
+
 
