59861103249f60ab131f3419f120d26e53a90f5f
Saba Safavi <sabasfvi@gmail.com>
2025-03-02 09:25:44 +0330

    12esfandmah

--- tamrinatSabzlearn/js/ticketrealproject.js
+++ tamrinatSabzlearn/js/ticketrealproject.js
@@ -8,14 +8,25 @@ let countries=document.getElementsByClassName('select-countries')[0];
 let citySelect=document.getElementsByClassName('select-cities')[0];
 
 countries.addEventListener('change', function(){
-    citySelect.innerHTML=''
-  let chosenCountry= cities[countries.value];  //iran[]
-  chosenCountry.forEach(function (City){
-      let optionCity=document.createElement("option");
-      optionCity.value=City;
-      optionCity.textContent=City;
-      citySelect.appendChild(optionCity);
+    if(countries.value==='please select'){
+        citySelect.innerHTML=''
+        citySelect.innerHTML+='<option>'+'plese select the citiy...'+'</option>'
+    }else {
+        citySelect.innerHTML=''
+        let chosenCountry= cities[countries.value];  //iran[]
+        chosenCountry.forEach(function (City){
 
-  })
+            citySelect.innerHTML+='<option value=City>'+City+'</option>'
+            // let optionCity=document.createElement("option");
+            // optionCity.value=City;
+            // optionCity.textContent=City;
+            // citySelect.appendChild(optionCity);
+
+        })
+
+
+
+    }
+
 
 })
