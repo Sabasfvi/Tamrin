1277b7d62a0ed1096f6cee7a8d5cf852d697bab0
Saba Safavi <sabasfvi@gmail.com>
2025-04-15 20:40:14 +0330

    26farvardin

---
+++ tamrinatSabzlearn/js/customContextMenu.js
@@ -0,0 +1,30 @@
+﻿const mainDivOfMenu=document.getElementById('contextMenu');
+
+document.body.addEventListener("keydown",keyHandler)
+document.body.addEventListener("click",clickHandler)
+document.body.addEventListener("contextmenu",contextHandler)
+
+function contextHandler(event) {
+   mainDivOfMenu.style.top=event.pageY+"px"
+   mainDivOfMenu.style.left=event.pageX+"px"
+   mainDivOfMenu.style.display='block'
+
+   event.preventDefault()
+}
+
+
+function keyHandler(event){
+   if(event.code === "Escape"){
+      mainDivOfMenu.style.display='none'
+   }
+}
+
+
+function clickHandler() {
+   mainDivOfMenu.style.display='none'
+}
+
+
+
+
+
